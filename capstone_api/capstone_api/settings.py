cffc4daf9a18e89cb8c8662fd616dde35b541b70
unknown <mrchrisfarrell@yahoo.com>
2021-08-25 16:20:55 -0400

    added simple_jwt- not working. will try djoser

--- capstone_api/capstone_api/settings.py
+++ capstone_api/capstone_api/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'companies.apps.CompaniesConfig',
     'accounts.apps.AccountsConfig',
     'corsheaders',
+    'rest_framework_simplejwt',
 ]
 
 MIDDLEWARE = [
@@ -125,6 +126,19 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+         'rest_framework.permissions.IsAuthenticated',
+         'rest_framework.permissions.IsAdminUser',
+         ],
+
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+}
+
+
 
 try:
     from capstone_api.local_settings import *
