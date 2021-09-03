638e4e99d47fc8857901aa630e07ea664008c974
unknown <mrchrisfarrell@yahoo.com>
2021-09-03 15:05:56 -0400

    updated models for front end

--- capstone_api/accounts/models.py
+++ capstone_api/accounts/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 # Create your models here.
 class User(AbstractUser):
     """Our custom user model that adds a new field to the default django user model"""
