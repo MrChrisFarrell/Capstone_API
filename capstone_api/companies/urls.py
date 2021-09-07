9ac1d426fe09871a3afa034efb62e6e0c65163c1
unknown <mrchrisfarrell@yahoo.com>
2021-09-07 19:12:11 -0400

    employee lat long urls and views fixed.

--- capstone_api/companies/urls.py
+++ capstone_api/companies/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('promotion/<int:pk>/', views.PromotionDetail.as_view()),
     path('employee/', views.EmployeeList.as_view()),
     path('employee/<int:pk>/', views.EmployeeDetail.as_view()),
-    path('complatlong/', views.CompanyLatLongList.as_view())
+    path('complatlong/', views.CompanyLatLongList.as_view()),
+    path('emplatlong/', views.EmployeeLatLongList.as_view()),
+    path('emplatlong/get/', views.EmployeeLatLongDetail.as_view()),
 ]
